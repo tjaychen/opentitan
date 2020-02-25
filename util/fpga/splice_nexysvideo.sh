51f2ebeb126819f34764f951e35aec00793420a2
Timothy Chen <timothytim@google.com>
2020-02-25 14:26:35 -0800

    [FPGA] Automatically dump mmi
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- util/fpga/splice_nexysvideo.sh
+++ util/fpga/splice_nexysvideo.sh
@@ -24,6 +24,7 @@ TARGET_EXPORT="${TARGET_PREFIX}/boot_rom_export_fpga_nexysvideo"
 TARGET="${BIN_DIR}/${TARGET_PREFIX}/boot_rom_fpga_nexysvideo"
 
 FPGA_BUILD_DIR=build/lowrisc_systems_top_earlgrey_nexysvideo_0.1/synth-vivado/
+FPGA_MMI_PATH=${FPGA_BUILD_DIR}/lowrisc_systems_top_earlgrey_nexysvideo_0.1.runs/impl_1
 FPGA_BIT_NAME=lowrisc_systems_top_earlgrey_nexysvideo_0.1
 
 ./meson_init.sh
@@ -39,7 +40,7 @@ util/fpga/addr4x.py -i "${TARGET}.brammem" -o "${TARGET}.mem"
 # the implemented design in Vivado and then inspecting the cell properties of
 # the corresponding BRAM cells. This information is very useful when debugging
 # the splicing flow.
-updatemem -force --meminfo util/fpga/bram_load.mmi \
+updatemem -force --meminfo "${FPGA_MMI_PATH}/rom.mmi" \
   --data "${TARGET}.mem" \
   --bit "${FPGA_BUILD_DIR}/${FPGA_BIT_NAME}.bit"  --proc dummy \
   --out "${FPGA_BUILD_DIR}/${FPGA_BIT_NAME}.splice.bit" \

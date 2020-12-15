bc2d9cfe183b1d2a13e8ada5d0f08d59643782cc
Timothy Chen <timothytim@google.com>
2020-12-14 17:20:51 -0800

    [keymgr / lc] Additional hook-ups
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [keymgr] Add init recognition to cfg_en and sideload controls
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- hw/ip/keymgr/dv/env/keymgr_if.sv
+++ hw/ip/keymgr/dv/env/keymgr_if.sv
@@ -7,7 +7,7 @@ interface keymgr_if(input clk, input rst_n);
 
   import keymgr_pkg::*;
 
-  lc_data_t lc;
+  lc_ctrl_pkg::lc_keymgr_div_t keymgr_div;
   otp_data_t otp;
   otp_ctrl_pkg::otp_keymgr_key_t otp_key;
   flash_ctrl_pkg::keymgr_flash_t flash;
@@ -18,7 +18,7 @@ interface keymgr_if(input clk, input rst_n);
   hw_key_req_t aes_key;
 
   task automatic init();
-    lc      = LC_DATA_DEFAULT;
+    keymgr_div = 64'h5CFBD765CE33F34E;
     otp     = OTP_DATA_DEFAULT;
     otp_key = otp_ctrl_pkg::OTP_KEYMGR_KEY_DEFAULT;
     flash   = flash_ctrl_pkg::KEYMGR_FLASH_DEFAULT;
